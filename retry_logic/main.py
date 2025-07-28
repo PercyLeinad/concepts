dab36032420dcdf8540955626fd577e8635bff45
Percy Daniel <102838909+PercyLeinad@users.noreply.github.com>
2025-07-28 13:48:41 +0300

    requests with backoff

---
+++ retry_logic/main.py
@@ -0,0 +1,27 @@
+import requests
+from urllib3.exceptions import ProtocolError
+import time
+import random
+
+url = 'http://localhost:8080/status/100,200,300,400,500'
+
+def get_response(url) -> int:
+    res = requests.get(url)
+    return res.status_code
+
+def backoff_fn(test_url,max_tries = 10):
+    for trial in range(1,max_tries+1):
+        try:
+            response = get_response(test_url)
+            if response == 200:
+                return f'Success {response}'
+            else:
+                raise ValueError(f"{response}")        
+        except (requests.RequestException, ProtocolError, ValueError) as e:
+            delay = random.randint(1,5)
+            print(f'Trial {trial}: {e} — Retrying after {delay}s...')
+            time.sleep(delay)
+
+    return "Failed after {} trials".format(max_tries)
+
+print(backoff_fn(url))
