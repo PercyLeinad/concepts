302b65344fe9c8d676cb16f914228159e5613598
Percy Daniel <102838909+PercyLeinad@users.noreply.github.com>
2025-08-07 14:17:27 +0300

    sample code

--- functional_prog/reduce.py
+++ functional_prog/reduce.py
@@ -1,7 +1,6 @@
 '''
 Python's reduce() is a function that implements a mathematical technique called folding or reduction. 
 reduce() is useful when you need to apply a function to an iterable and reduce it to a single cumulative value. 
-Python's reduce() is popular among developers with a functional programming background, but Python has more to offer.
 '''
 from functools import reduce
 
